a4ee6e199311cc5a7e3068e1f638f9cf8daea55b
naotaro0123 <n.ikewada@gmail.com>
2019-06-22 18:12:32 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # webgl-naotaro-template
-my webgl template
+my webgl simple template
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm i
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+
+# Lint Fix
+$ npm run lint
+
+```
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+    watchContentBase: true,
+    port: 3000,
+    open: true,
+    openPage: './index.html'
+  }
+};
